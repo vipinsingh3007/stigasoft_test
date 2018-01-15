c23cf762a90004b53a0ec2db623990e25dc86dc1
Vipinkumar Singh <vipinsingh3007@gmail.com>
2018-01-16 02:32:17 +0530

    Final commit for the test project

--- components/AutomatedSlider.js
+++ components/AutomatedSlider.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { View,Text,StyleSheet } from "react-native";
+import { View,Text,StyleSheet,Animated } from "react-native";
 import ImageSlider from 'react-native-image-slider';
 
+const message = ['This is Message1', 'This is message2'];
 
 export default class AutomatedSlider extends React.Component{
 
@@ -11,25 +12,34 @@ export default class AutomatedSlider extends React.Component{
 
   state = {
             position: 1,
-            interval: null
+            interval: null,
+            msgInterval:null,
+            message:message[0]
         };
 
   componentWillMount() {
        this.setState({interval: setInterval(() => {
-           this.setState({position: this.state.position === 2 ? 0 : this.state.position + 1});
+           this.setState({position: this.state.position === 2 ? 0 : this.state.position + 1,});
        }, 5000)});
+
+       this.setState({ msgInterval: setInterval(() => {
+            this.setState( { message:this.state.message === message[1] ? message[0] : message[1]})
+          }, 2500)});
+
    }
  componentWillUnmount() {
        clearInterval(this.state.interval);
+       clearInterval(this.state.msgInterval);
    }
 
   render(){
     return (
-      <View>
-        <ImageSlider images={[
-         'https://placeimg.com/640/480/any',
-         'https://placeimg.com/640/480/any',
-         'https://placeimg.com/640/480/any'
+      <View style={styles.container}>
+      <Animated.Text>{this.state.message}</Animated.Text>
+      <ImageSlider images={[
+         'https://placeimg.com/640/480/animals',
+         'https://placeimg.com/640/480/nature',
+         'https://placeimg.com/640/480/arch/grayscale'
         ]} position={this.state.position}
            onPositionChanged={position => this.setState({position})}/>
 
@@ -41,8 +51,10 @@ export default class AutomatedSlider extends React.Component{
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    justifyContent:'center',
-    alignItems:'center',
-    backgroundColor:"#fff"
+    alignItems:'stretch',
+    backgroundColor:"#fff",
+  },
+  imageText:{
+    justifyContent:'center'
   }
 });
